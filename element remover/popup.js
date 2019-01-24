59c075e8b26b4931c3bd9cda56cab1923a6b7c0d
Eugene Moon <uoojin95@gmail.com>
2019-01-24 00:48:34 -0500

    chrome extension example

---
+++ element remover/popup.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // document.querySelector('button').addEventListener('click', onclick ,false)
+
+    // function onclick () {
+    //     chrome.tabs.query({currentWindow: true, active: true}, (tabs) => {
+    //         chrome.tabs.sendMessage(tabs[0].id, tabs, setCount)
+    //     })
+    // }
+
+    // function setCount (res) {
+    //     const div = document.createElement('div')
+    //     div.textContent = `${res.count}`
+    //     document.body.appendChild(div)
+    // }
+
+    const bg = chrome.extension.getBackgroundPage()
+    Object.keys(bg.bears).forEach(function(url) {
+        const div = document.createElement('div')
+        div.textContent = `${url}: ${bg.bears[url]}`
+        document.body.appendChild(div)
+    })
+
+}, false)
