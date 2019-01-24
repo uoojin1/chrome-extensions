a2dd607d4b53bb137ad5b056ce5171bab5ebda6b
Eugene Moon <uoojin95@gmail.com>
2019-01-24 02:20:02 -0500

    now working on the element remover

--- element remover/background.js
+++ element remover/background.js
@@ -1,12 +1,12 @@
 // window.bears = {}
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    // window.bears[request.url] = request.count
-})
+// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+//     // window.bears[request.url] = request.count
+// })
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-    // chrome.tabs.create({
-    //     url: 'popup.html'
-    // })
-    alert("should enable remover")
-})
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//     // chrome.tabs.create({
+//     //     url: 'popup.html'
+//     // })
+//     alert("should enable remover")
+// })
